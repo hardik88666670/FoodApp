a36f09528ccb952432d0b31641ed816c371b1752
hardik chavda <ch5191617@gmail.com>
2025-07-30 22:57:47 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -13,4 +13,22 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
-GiToaster.success("")
+// GiToaster.success("")
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import { Provider } from 'react-redux';
+// import { store } from './Redux/store';
+// import { Toaster } from 'react-hot-toast';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//       <Toaster position="top-right" reverseOrder={false} />
+//     </Provider>
+//   </React.StrictMode>
+// );
